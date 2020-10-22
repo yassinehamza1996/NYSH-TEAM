1869eac38cbbe08f1a3bb6d61b02ecf62ad3e692
siwaramr98 <siwaramairi98@gmail.com>
2020-10-22 18:58:19 +0100

    nouvel ajout par siwar

--- src/bookstore/dao/Daocategorie.java
+++ src/bookstore/dao/Daocategorie.java
@@ -4,6 +4,7 @@ public class Daocategorie {
 
 public void create_categorie(String Categorie) {
 	System.out.println("la cetegorie est"+Categorie);
+	System.out.println("siwar ajoute une nouvelle ligne");
 }	
 	
 }
