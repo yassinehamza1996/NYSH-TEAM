734c07ea2c029fbcd30744c32eb81b488fbe835b
yassinehamza1996 <yassine.hamza@etudiant-fst.utm.tn>
2020-10-22 15:27:29 +0200

    sys out

--- src/bookstore/dao/Daocategorie.java
+++ src/bookstore/dao/Daocategorie.java
@@ -4,6 +4,7 @@ public class Daocategorie {
 
 public void create_categorie(String Categorie) {
 	System.out.println("la cetegorie est"+Categorie);
+	System.out.println("this is the last one");
 }	
 	
 }
